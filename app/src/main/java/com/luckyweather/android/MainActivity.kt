fe88fd04fdc0e9d2f95f5b786546b00b862fbbd0
Luke <Luke-Chu@outlook.com>
2022-07-16 10:59:12 +0800

    实现搜索全球城市数据功能

--- app/src/main/java/com/luckyweather/android/MainActivity.kt
+++ app/src/main/java/com/luckyweather/android/MainActivity.kt
@@ -2,10 +2,13 @@ package com.luckyweather.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.luckyweather.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
     }
 }
